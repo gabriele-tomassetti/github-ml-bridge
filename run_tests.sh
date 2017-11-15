1523d67aec008f858979ecd9b6eddc6a40cf11d5
Gabriele <unosviluppatore@users.noreply.github.com>
2017-11-15 21:43:55 +0100

    cleaning temporary files for tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 cd test
-# start the fake server
+# start the fake mail server
 twistd localmail --imap 2000 --smtp 3000 --http 4000 --file localmail.mbox
 
 # run tests
 nose2
 
-# kill the fake server
+# kill the fake mail server
 ps -ef | grep "twistd localmail" | grep -v grep | awk '{print $2}' | xargs kill
+
+# clean the fake mail server data
+rm ./localmail.mbox
 
 cd ..
